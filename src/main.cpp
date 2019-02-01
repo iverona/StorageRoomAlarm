5188176d1edeb306b589911b340101634d4cbfba
iverona <iverona@estudio-verona.com>
2019-02-01 14:47:09 +0100

    Added OPEN-CLOSE information

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <SigFox.h>
 #include <ArduinoLowPower.h>
 
-void alarmIdGenerator();
+#define WAKE_UP_INTERVAL 259200000 //3 days
+
+float readBatteryLevel();
+void alarmInterruption();
 void reboot();
 
-long alarmId;
+char alarmCode = '0';
 
 void setup()
 {
@@ -12,7 +15,6 @@ void setup()
   // We are using Serial1 instead than Serial because we are going in standby
   // and the USB port could get confused during wakeup. To read the debug prints,
   // connect pins 13-14 (TX-RX) to a 3.3V USB-to-serial converter
-
   Serial1.begin(115200);
   while (!Serial1)
   {
@@ -32,7 +34,8 @@ void setup()
 
   // attach pin 0 and 1 to a switch and enable the interrupt on voltage falling event
   pinMode(0, INPUT_PULLDOWN);
-  LowPower.attachInterruptWakeup(0, alarmIdGenerator, RISING);
+  LowPower.attachInterruptWakeup(0, alarmInterruption, CHANGE);
+  //LowPower.attachInterruptWakeup(0, alarmInterruptionClose, FALLING);
 }
 
 void loop()
@@ -45,20 +48,27 @@ void loop()
 
   delay(100);
 
-  // 1 bytes (ALM) + 4 bytes (long alarmId) < 12 bytes
-  String to_be_sent = "1"; // + String(alarmId);
+  // 2 bytes (int alarmCode) + 4 bytes (float batt. level) < 12 bytes
+  String to_be_sent = String(alarmCode) + String(readBatteryLevel());
 
   SigFox.beginPacket();
   SigFox.print(to_be_sent);
   int ret = SigFox.endPacket();
 
+  //alarmCode = 0;
+
   // shut down module, back to standby
   SigFox.end();
 }
 
-void alarmIdGenerator()
+void alarmInterruption()
 {
-  alarmId = random(1000);
+  int state = digitalRead(0);
+
+  if (state == HIGH)
+    alarmCode = '1';
+  else
+    alarmCode = '0';
 }
 
 void reboot()
@@ -66,4 +76,17 @@ void reboot()
   NVIC_SystemReset();
   while (1)
     ;
+}
+
+float readBatteryLevel()
+{
+  analogReadResolution(10);
+  analogReference(AR_INTERNAL1V0); //AR_DEFAULT: the default analog reference of 3.3V // AR_INTERNAL1V0: a built-in 1.0V reference
+
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(ADC_BATTERY);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 4.3V):
+  float voltage = sensorValue * (3.25 / 1023.0);
+
+  return voltage;
 }
